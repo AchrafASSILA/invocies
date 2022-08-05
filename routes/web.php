42b8ed0471129e37de15399d867b8cc685c024a8
AchrafASSILA <achrafassila678@gmail.com>
2022-08-04 20:26:15 -0700

    upload

--- routes/web.php
+++ routes/web.php
@@ -30,27 +30,27 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
-    })->name('dashboard');
+    })->middleware('isactive')->name('dashboard');
 });
 
 Route::prefix('admin')->group(function () {
 
-    Route::resource('invoices', InvoicesController::class)->middleware('auth');
-    Route::resource('products', ProductsController::class)->middleware('auth');
-    Route::resource('sections', SectionsController::class)->middleware('auth');
-    Route::resource('invoicesAtt', InvoicesAttachmentsController::class)->middleware('auth');
+    Route::resource('invoices', InvoicesController::class)->middleware(['auth', 'isactive']);
+    Route::resource('products', ProductsController::class)->middleware(['auth', 'isactive']);
+    Route::resource('sections', SectionsController::class)->middleware(['auth', 'isactive']);
+    Route::resource('invoicesAtt', InvoicesAttachmentsController::class)->middleware(['auth', 'isactive']);
     Route::get('/invoice_reports', [InvoiceReportController::class, 'index'])->name('invoice_report');
-    Route::get('/invoiceArchive', [InvoicesController::class, 'getInvoicesArchived'])->name('invoiceArchive')->middleware('auth');
-    Route::get('/markAsRead', [InvoicesController::class, 'markAllNotificationsAsRead'])->name('markAsRead')->middleware('auth');
-    Route::get('/print_invoice/{id}', [InvoicesController::class, 'printInvoice'])->name('printInvoice')->middleware('auth');
-    Route::put('/restoreInvoice/{id}', [InvoicesController::class, 'restoreInvoice'])->name('restoreInvoice')->middleware('auth');
-    Route::post('/invoice/{id}', [InvoicesController::class, 'updateStatus'])->name('updateStatus')->middleware('auth');
-    Route::delete('/invoice/{id}', [InvoicesController::class, 'transformToArchived'])->name('transformToArchived')->middleware('auth');
-    Route::post('/search_invoice', [InvoiceReportController::class, 'Search_invoices'])->name('Search_invoices')->middleware('auth');
-    Route::post('/search_customer', [UserReportControler::class, 'Search_customers'])->name('Search_customers')->middleware('auth');
-    Route::get('/section/{id}', [InvoicesController::class, 'getproducts'])->middleware('auth');
+    Route::get('/invoiceArchive', [InvoicesController::class, 'getInvoicesArchived'])->name('invoiceArchive')->middleware(['auth', 'isactive']);
+    Route::get('/markAsRead', [InvoicesController::class, 'markAllNotificationsAsRead'])->name('markAsRead')->middleware(['auth', 'isactive']);
+    Route::get('/print_invoice/{id}', [InvoicesController::class, 'printInvoice'])->name('printInvoice')->middleware(['auth', 'isactive']);
+    Route::put('/restoreInvoice/{id}', [InvoicesController::class, 'restoreInvoice'])->name('restoreInvoice')->middleware(['auth', 'isactive']);
+    Route::post('/invoice/{id}', [InvoicesController::class, 'updateStatus'])->name('updateStatus')->middleware(['auth', 'isactive']);
+    Route::delete('/invoice/{id}', [InvoicesController::class, 'transformToArchived'])->name('transformToArchived')->middleware(['auth', 'isactive']);
+    Route::post('/search_invoice', [InvoiceReportController::class, 'Search_invoices'])->name('Search_invoices')->middleware(['auth', 'isactive']);
+    Route::post('/search_customer', [UserReportControler::class, 'Search_customers'])->name('Search_customers')->middleware(['auth', 'isactive']);
+    Route::get('/section/{id}', [InvoicesController::class, 'getproducts'])->middleware(['auth', 'isactive']);
     Route::resource('users', UserController::class);
     Route::get('/users_report', [UserReportControler::class, 'index'])->name('user_report');
     Route::get('/{page}', [AdminController::class, 'index'])->middleware('auth');
-    Route::get('/invoices_status/{status}', [InvoicesController::class, 'getInvoicesByStatus'])->name('invoiceStatus')->middleware('auth');
+    Route::get('/invoices_status/{status}', [InvoicesController::class, 'getInvoicesByStatus'])->name('invoiceStatus')->middleware(['auth', 'isactive']);
 });
